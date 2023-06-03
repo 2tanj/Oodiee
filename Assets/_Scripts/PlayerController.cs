ed91971eb115c62ff5766a3891ba40f07d20a233
Aleksandar Golubovic <golubovic.aleksandar01@gmail.com>
2023-06-03 12:28:55 +0200

    Fish steering

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,27 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody _playerRb;
-
-    [SerializeField]
-    private float _speed = .5f;
-
-    private void Start()
-    {
-        _playerRb = GetComponent<Rigidbody>();
-    }
+    [SerializeField, Range(0,5)]
+    private float _speed = .5f, _steerSpeed = .1f;
 
     private void Update()
     {
-        //var newPos = _player.transform.position;
-        //newPos.z += _speed;
-        //_player.transform.position = newPos;
-        var pos = transform.position;
-        pos.z += _speed;
-        _playerRb.AddForce(transform.up * _speed);
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
 
+        Swim(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")));
+    }
+
+
+    private void Swim(Vector2 newPos)
+    {
+        var pos = transform.position;
+
+        pos.x += newPos.x * _steerSpeed;
+        pos.y += newPos.y * _steerSpeed;
+        
+        pos.z += _speed;
+
+        transform.position = pos;
     }
 }
