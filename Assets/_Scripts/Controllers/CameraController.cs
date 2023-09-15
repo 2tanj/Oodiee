dabc2b0c4a58d3447143282de674efbd1eb0301e
Aleksandar Golubovic <golubovic.aleksandar01@gmail.com>
2023-09-15 03:06:45 +0200

    camera center fixed to player

--- Assets/_Scripts/Controllers/CameraController.cs
+++ Assets/_Scripts/Controllers/CameraController.cs
@@ -1,24 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField]
-    private PlayerController _player;
+    CinemachineVirtualCamera _camera;
+    CinemachineFramingTransposer _cameraBody;
 
-    [SerializeField]
-    private float _distanceFromPlayer, _positionOffset = 7;
+    //[SerializeField]
+    //private PlayerController _player;
 
+    //[SerializeField]
+    //private float _distanceFromPlayer, _positionOffset = 7;
+
+    private void Start()
+    {
+        _camera = GetComponent<CinemachineVirtualCamera>();
+        _cameraBody = _camera.GetCinemachineComponent<CinemachineFramingTransposer>();
+    }
 
     private void Update()
     {
-        var pos = transform.position;
-        
-        pos.x = _player.transform.position.x/* / _positionOffset*/;
-        pos.z = _player.transform.position.z / _positionOffset;
-        pos.y = (_player.transform.position.y + _distanceFromPlayer);
+        _cameraBody.m_ScreenX = .5f;
+        _cameraBody.m_ScreenY = .5f;
 
-        transform.position = pos;
+        _cameraBody.m_DeadZoneWidth = 0f;
+        _cameraBody.m_DeadZoneHeight = 0f;
+
+        //var pos = transform.position;
+        
+        //pos.x = _player.transform.position.x/* / _positionOffset*/;
+        //pos.z = _player.transform.position.z / _positionOffset;
+        //pos.y = (_player.transform.position.y + _distanceFromPlayer);
+
+        //transform.position = pos;
     }
 }
