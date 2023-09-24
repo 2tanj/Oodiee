b7709e3a560ac864559aa10be035e948bba286dc
Aleksandar Golubovic <golubovic.aleksandar01@gmail.com>
2023-09-25 01:53:26 +0200

    unity service packages install

---
+++ Assets/_Scripts/Unity Services/PlayerAuthentication.cs
@@ -0,0 +1,54 @@
+using System.Threading.Tasks;
+using UnityEngine;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using System;
+
+public static class PlayerAuthentication
+{
+    private static readonly IAuthenticationService _service =
+        AuthenticationService.Instance;
+
+    // player info is stored locally
+    // player loses progress on app uninstall
+    public static async Task SignInAnonymouslyAsync()
+    {
+        if (_service.IsSignedIn)
+        {
+            Debug.Log("Player is already signed in.");
+            return;
+        }
+
+        try
+        {
+            await _service.SignInAnonymouslyAsync();
+            Debug.Log("Successful anonymous sign in");
+        }
+        catch (AuthenticationException e)
+        {
+            Debug.LogException(e);
+        }
+        catch (RequestFailedException e)
+        {
+            Debug.LogException(e);
+        }
+    }
+
+    public static async Task SignOutAsync()
+    {
+        try
+        {
+            _service.SignOut();
+            await Task.CompletedTask;
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+        }
+    }
+
+    public static async Task DeleteAccountAsync() =>
+        await _service.DeleteAccountAsync();
+
+    public static string GetPlayerId() => _service.PlayerId;
+}
